0498105ebc24a83fbbf6473e1eac76c09ebe091d
Takeshi Kaneko <target.ark@gmail.com>
2019-05-08 23:47:04 +0900

    Fix 404

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   productionSourceMap: false,
   lintOnSave: false,
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/simblock-visualizer/" : "/",
   // workaround for:
   //   https://github.com/cvalenzuela/Mappa/issues/22
   configureWebpack: config => {
